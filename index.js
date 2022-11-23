5ec8c1bb4c42e9e7540f7e239cd216a769f004d5
dorecio <dorisrecio@gmail.com>
2022-11-22 23:16:06 -0600

    se agrega archivo index.js con validaciones de datos de entrada

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const generateMarkdown = require('./src/script');
+const { validateEmail } = require('./validation/validate');
+const { validateNum } = require('./validation/validate');
+const { validateString } = require('./validation/validate');
 
 const employeeArray = [];
 
@@ -14,24 +17,24 @@ const managerData = [
         name: 'managerName'
     },
     {
-        type: 'input',
+        type: 'number',
         message: '¿Cuál es el identificador de este empleado? ',
         name: 'id'
     },
     {
-        type: 'input',
+        type: 'email',
         message: '¿Cuál es su correo electrónico? ',
         name: 'email'
     },
 
     {
-        type: 'input',
+        type: 'number',
         message: '¿Cuál es el número de oficina de este emplado? ',
         name: 'numOficina'
     },
 ];
 
-const menu =[
+const menu = [
 
     {
         type: 'list',
@@ -50,17 +53,17 @@ const op1 = [
         type: 'input',
         message: '¿Cuál es el nombre del ingeniero ? ',
         name: 'engineerName'
-    }, 
+    },
     {
-        type: 'input',
+        type: 'number',
         message: '¿Cuál es el identificador de este empleado? ',
         name: 'id'
     },
     {
-        type: 'input',
+        type: 'email',
         message: '¿Cuál es su correo electrónico? ',
         name: 'email'
-    },  
+    },
     {
         type: 'input',
         message: '¿Cuál es el nombre de usuario de GitHub? ',
@@ -75,12 +78,12 @@ const op2 = [
         name: 'pasanteName'
     },
     {
-        type: 'input',
+        type: 'number',
         message: '¿Cuál es el identificador de este pasante? ',
         name: 'id'
     },
     {
-        type: 'input',
+        type: 'email',
         message: '¿Cuál es su correo electrónico? ',
         name: 'email'
     },
@@ -88,18 +91,14 @@ const op2 = [
         type: 'input',
         message: '¿Cuál es la escuela del pasante? ',
         name: 'escuela'
-    }, 
+    },
 ]
 
-   
+
 function writeToFile(archivo, codigo) {
     fs.writeFileSync(archivo, codigo);
- }
+}
 
-// TODO: Crear una función para escribir el archivo HTML
-//function writeToFile(fileName, html) {
-//    fs.writeFileSync(fileName, html);
-//}
 
 // TODO: Crear una función para inicializar la aplicación
 const init = () => {
@@ -108,49 +107,50 @@ const init = () => {
     console.log('     Forma tu equipo de ingeniería de software    ',);
     console.log('==================================================',);
     inquirer.prompt(managerData).then((data) => {
-        console.log('DATA ',data);
-        const gerente = new Manager(data.managerName,data.id,data.email,data.numOficina);
-        console.log('GERENTE  ====>', gerente);
+        const nombre = validateString(data.managerName);
+        const identificador = validateNum(data.id);
+        const correo = validateEmail(data.email);
+        const numeroOficina = validateNum(data.numOficina);
+        const gerente = new Manager(nombre, identificador, correo, numeroOficina);
         // guardar objeto en arreglo de empleados
         employeeArray.push(gerente);
-      
     }).then(() => opcionMenu());
 }
 
 function opcionMenu() {
     inquirer.prompt(menu).then((menuOpc) => {
-        console.log(menuOpc);
         if (menuOpc.agregar == 'Agregar un ingeniero') {
             agregarIngeniero();
         } else if (menuOpc.agregar == 'Agregar un pasante') {
             agregarPasante();
         } else {
-            console.log(employeeArray);
             // generar el html  
             writeToFile('./dist/empleados.html', generateMarkdown(employeeArray));
         }
     });
 }
- 
+
 function agregarIngeniero() {
     inquirer.prompt(op1).then((op1Data) => {
-        console.log('DATOS',op1Data);
-        const ingeniero = new Engineer(op1Data.engineerName, op1Data.id, op1Data.email, op1Data.gitHub);
-        console.log('INGENIERO  ====>', ingeniero);
+        const nombre = validateString(op1Data.engineerName);
+        const identificador = validateNum(op1Data.id);
+        const correo = validateEmail(op1Data.email);
+        const git = validateString(op1Data.gitHub);
+        const ingeniero = new Engineer(nombre, identificador, correo, git);
         // guardar objeto en arreglo de empleados
         employeeArray.push(ingeniero);
-      
     }).then(() => opcionMenu());
 }
 
 function agregarPasante() {
     inquirer.prompt(op2).then((op2Data) => {
-        console.log('DATO_PAS',op2Data);
-        const pasante = new Intern(op2Data.pasanteName, op2Data.id, op2Data.email, op2Data.escuela);
-        console.log('PASANTE  ====>', pasante);
+        const nombre = validateString(op2Data.pasanteName);
+        const identificador = validateNum(op2Data.id);
+        const correo = validateEmail(op2Data.email);
+        const escuela = validateString(op2Data.escuela);
+        const pasante = new Intern(nombre, identificador, correo, escuela);
         // guardar objeto en arreglo de empleados
         employeeArray.push(pasante);
-    
     }).then(() => opcionMenu());
 }
 
